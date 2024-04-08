97b91a5f28e47e80c6390b22ff4b0de1161b1dc2
kuzmishchew <kuzmishchew@gmail.com>
2024-04-08 18:17:36 +0500

    feat: done Циклы — Упражнение 3

---
+++ Циклы/Упражнение 3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая выводит на консоль таблицу умножения/
+*/
+namespace Упражнение_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Таблица умножения \n");
+
+        for (int i = 1; i <= 9; i = i + 3)
+        {
+            for (int j = 1; j <= 9; ++j) 
+            {
+                for (int c = 0; c < 3; ++c)
+                    Console.Write($"{i + c} * {j} = {(i + c) * j}\t");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+        }
+
+        Console.ReadKey();
+    }
+}
