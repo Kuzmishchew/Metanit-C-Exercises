eefcf4a2678325e1fd0dba44b238672b64ac927f
kuzmishchew <kuzmishchew@gmail.com>
2024-04-09 21:23:00 +0500

    feat: add Fibonachi method

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Fibonachi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int fibPos;
+        long fibNumber;
+
+        Console.Write("Введите номер позиции: ");
+        fibPos = Convert.ToInt32(Console.ReadLine());
+
+        fibNumber = GetFibNumber(fibPos);
+
+        Console.WriteLine(fibNumber);
+
+        long GetFibNumber(int pos) {
+            long result = 0;
+
+            if (pos == 0)
+                return 0;
+            else if (pos == 1)
+                return 1;
+
+            
+            result = GetFibNumber(pos - 1) + GetFibNumber(pos - 2);
+                 
+
+            return result;
+        }
+    }
+
+    
+}
