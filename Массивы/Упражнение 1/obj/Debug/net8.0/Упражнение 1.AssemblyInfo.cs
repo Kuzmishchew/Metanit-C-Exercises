37d838f29e6a174c637f55d4cc22bc2a3bceeb47
kuzmishchew <kuzmishchew@gmail.com>
2024-04-09 19:28:10 +0500

    feat: done Массивы — Упражнение 1

--- Массивы/Упражнение 1/obj/Debug/net8.0/Упражнение 1.AssemblyInfo.cs
+++ Массивы/Упражнение 1/obj/Debug/net8.0/Упражнение 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Упражнение 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b633bde796e1d516b302334ce22b6d23f593954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719ec5e2192f62b473a0dcda8481dc947d25bfae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Упражнение 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Упражнение 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
